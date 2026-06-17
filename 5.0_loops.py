910428285280d2c5166051a89f2efc5f613ffd5c
Arup Roy <aruproy9003@gmail.com>
2026-06-18 01:23:29 +0530

    Add for loop and while loop examples
    Added examples of for and while loops in Python.

--- 5.0_loops.py
+++ 5.0_loops.py
@@ -1,2 +1,13 @@
+#for loop (n-1)
 for i in range(5):
     print(i)
+#output: 0 1 2 3 4    
+
+
+#while loop (starting value + condition + update)
+count = 1                   #starting value
+
+while count <= 5:           #condition
+    print(count)
+    count += 1              #update
+#output: 1 2 3 4 5
