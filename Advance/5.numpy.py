7c31f6ee63a72af7f73d9b99c9f30a4d3db03675
Arup Roy <aruproy9003@gmail.com>
2026-06-18 18:32:57 +0530

    Create 5.numpy.py

---
+++ Advance/5.numpy.py
@@ -0,0 +1,12 @@
+"""
+NumPy is a Python library for:
+Mathematical calculations
+Large arrays
+Scientific computing
+"""
+
+import numpy as np
+arr = np.array([1, 2, 3, 4])
+print(arr)
+
+#output: [1 2 3 4]
