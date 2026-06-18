b158c0742245d31002ce36b0c7ba48904a5785ea
Arup Roy <aruproy9003@gmail.com>
2026-06-18 18:34:23 +0530

    Update 5.numpy.py

--- Advance/5.numpy.py
+++ Advance/5.numpy.py
@@ -8,5 +8,9 @@ Scientific computing
 import numpy as np
 arr = np.array([1, 2, 3, 4])
 print(arr)
-
 #output: [1 2 3 4]
+
+import numpy as np
+arr = np.array([1, 2, 3])
+print(arr * 2)
+#output: [2 4 6]
